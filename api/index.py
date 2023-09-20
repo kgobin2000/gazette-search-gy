490b1db31e8711c84295546ffdcc16b1b66ef86d
Khalid Gobin <khalidgobin@hotmail.com>
2023-09-20 12:37:42 -0400

    update build

--- api/index.py
+++ api/index.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import threading
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+import worker
+
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Import routes after app is created to avoid circular imports
+from routes import *
+
+if __name__ == "__main__":
+    # Start the worker thread
+    threading.Thread(target=worker.worker).start()
+    app.run()
